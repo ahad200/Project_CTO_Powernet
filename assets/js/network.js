ac666faf87c37daf2fd662a27371929105d0964b
ahad200 <diyas189@gmail.com>
2024-12-19 21:52:08 +0700

    "Update Network"

--- assets/js/network.js
+++ assets/js/network.js
@@ -3,3 +3,24 @@ $(document).ready(() => {
         jump('section:eq(1)')
     })    
 });
+
+$(document).ready(() => {
+    $('#hero-more').on('click',() => {
+        jump('section:eq(1)')
+    })    
+    servicesSetup();
+    AOS.init();
+});
+
+
+servicesSetup = function() {        
+    $('.service').each(function() {        
+        $(this).find('.service-nav-item').on('click', (e) => {
+            index = $(e.currentTarget).index();                        
+            $(this).find('.service-nav-item.active').removeClass('active');
+            $(this).find('.service-feature.active').removeClass('active');
+            $(this).find(`.service-nav-item:eq(${index})`).addClass('active');        
+            $(this).find(`.service-feature:eq(${index})`).addClass('active');        
+        })
+    });    
+}
